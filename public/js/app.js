0d5938779a4c530ed50add67a2e24d83850850ac
reolander <16732636+reolander@users.noreply.github.com>
2023-02-11 15:03:00 +0530

    code refactor

--- public/js/app.js
+++ public/js/app.js
@@ -20,8 +20,7 @@ weatherForm.addEventListener('submit', (event) => {
   
   const location = search.value
  // fetch('http://localhost:3000/weather?address='+ location)
- fetch('/weather?address='+ location)
-  .then((response) => {
+ fetch('/weather?address='+ location).then((response) => {
     response.json()
       .then((data) => {
         if(data.error) {
